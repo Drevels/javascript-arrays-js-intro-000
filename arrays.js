6951c7091c4eb39a7b1cd01041b5490dbe9774b1
danielle revels <ddaniellerrevels@aol.com>
2020-07-28 19:08:54 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand","kitkat","skittles"];
 function addElementToBeginningOfArray(array, element){
- var newarray = array.unshift()
+ var newarray = array
+ .unshift()
   return array
 }
 function destructivelyRemoveElementFromBeginningOfArray(array){
